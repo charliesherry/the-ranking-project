486a5e67839f9201f7a742fce7c07269195704dd
Carlos Glez <carlosggordon@hotmail.com>
2020-09-25 12:56:54 +0200

    borrar archivos antiguos

--- api/src/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask("apironhack")
-
-@app.route('/')
-def welcome():
-    return 'Welcome to the ApIronhack!'
-
--- api/src/server.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-
-PORT = 3000
-app.run("0.0.0.0", PORT, debug=True)
